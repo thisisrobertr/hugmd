d14e16ff27a805a7e7249bd64ecf4a26f368b29d
Robert <rdryder@wm.edu>
2024-11-21 02:51:49 -0500

    update prompts

--- pipelines.py
+++ pipelines.py
@@ -1,16 +1,16 @@
 import torch
 from transformers import pipeline
 
-pipe = pipeline("text2text-generation", model="google/flan-t5-large")
+pipe = pipeline("text2text-generation", model="google/flan-t5-large", device=0)
 
 # TODO: interactions with data to properly provide accurate information.
 
 # TODO: testing prompts in depth
 def gen_query(user_inp, anx):
     output = pipe(f"""
-    You are a health anxiety chatbot that is serving a user. Give a reasonable assessment of the following query: {user_inp}
+    You are a health anxiety chatbot that is serving a user. Give a reasonable assessment of the following query, and elaborate upon your reasoning without giving the prompt back: {user_inp}
     \nContext: The user is current anxious level {anx} on a scale of 1 to 10, so remain mindful of tone.
-    
+    The user wants to be addressed conversationally, so use the pronoun "you"
     Remind the user that you cannot diagnose as a chatbot if asked anything pertaining diagnosis.""", max_length = 500)
     return output[0]['generated_text']
 
